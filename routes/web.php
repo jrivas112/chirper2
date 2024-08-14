66aa359359e5c9aef20d54bd6b1ee951956b8ba2
jrivas112 <147008410+jrivas112@users.noreply.github.com>
2024-08-13 23:29:01 -0700

    Learning about Route::resource

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/test', function () {
+    return "test";
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('chirp', ChirpController::class)->only(['index', 'store'])->middleware(['auth','verified']);
 
 require __DIR__.'/auth.php';
